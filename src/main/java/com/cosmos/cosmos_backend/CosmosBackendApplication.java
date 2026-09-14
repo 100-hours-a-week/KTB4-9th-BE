77503b8f5c005726a4817c611d075396a6f0a99a
keryn.hwang <hyyy8810@gmail.com>
2026-09-15 01:01:23 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/cosmos/cosmos_backend/CosmosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos.cosmos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmos/cosmos_backend/CosmosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmos.cosmos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
